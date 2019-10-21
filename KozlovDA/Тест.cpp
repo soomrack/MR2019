b562e19c0d6c95f0c97cb31dbaa2167456562a45
Денис Козлов <deni.kozlov2016@yandex.ru>
2019-10-21 17:06:25 +0300

    0

--- KozlovDA/Тест.cpp
+++ KozlovDA/Тест.cpp
@@ -1,25 +1,8 @@
 ﻿#include <iostream>
 #include <ctime>
 #include <windows.h>
+#include "TestLib.h"
 using namespace std;
-
-void ba_dom_tss(int dots) {
-	for (int i = 0; i < dots; i++) {
-		cout << ". ";
-		Sleep(1000);
-	}
-	cout << endl;
-}
-
-void processing_of_question(char right_answer, char answer) {
-	setlocale(LC_ALL, "Russian");
-	while (answer != right_answer) {
-		cout << "Неверно. Попробуйте еще раз." << endl;
-		cin >> answer;
-	}
-	cout << "Верно\n" << endl;
-	Sleep(500);
-}
 
 int main() {
 	setlocale(LC_ALL, "Russian");
