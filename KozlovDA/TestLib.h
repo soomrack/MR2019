54b09648347429759f76dc45714d03667e8bb25d
dekoden <54942408+dekoden@users.noreply.github.com>
2019-10-21 16:24:52 +0300

    Добавил библиотеку

---
+++ KozlovDA/TestLib.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <windows.h>
+using namespace std;
+
+void ba_dom_tss(int dots);
+void processing_of_question(char right_answer, char answer);
