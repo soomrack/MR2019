affa579fd0e124baae1eff695fbe34aa10e54c2e
Fuxtrel <a99som@yandex.ru>
2019-10-29 03:15:14 +0300

    Rework to class type

--- SomovAS/main.cpp
+++ SomovAS/main.cpp
@@ -3,6 +3,5 @@
 #include <clocale>
 using namespace std;
 int main(){
-    setlocale(LC_ALL,"RUS");
     test();
 }
--- SomovAS/test.cpp
+++ SomovAS/test.cpp
@@ -2,59 +2,64 @@
 #include <iostream>
 #include <clocale>
 using namespace std;
-
-void test(){
-    string name, ans;
+class Question1{
+public:
+    string name;
     int count;
-
+    string question;
+    string ans;
+    string ans1;
+    string ans2;
+    string ans3;
+    string ans4;
+    void foo();
+};
+Question1 Question1(string question = "What scientist wrote the book <Origin of species>?", string ans1 = "Charls Darwin", string ans2 = "Aristotel")
+Question1 Question1(string ans3 = "Thomas Edison", string ans4 = "Alexandr Greyam Bell")
+Question1::foo(){
+    cout << Question1.question << endl;
+    cout << Question1.ans1 << endl << Question1.ans2 << endl << Question1.ans3 << endl << Question1.ans4 << endl;
+    cin >> ans;
+    while(ans != Question1.ans1){
+        cout << "Wrong, try again" << endl;
+        cin >> ans;
+    }
+    cout << "Well, lets continue" << endl;
+    Question1.count++;
+}
+class Question2:Question1{
+public:
+    Question1 Question1(string question = "This Belgian Catholic priest was the first to propose the big Bang theory",\
+    string ans1 = "Jorj Lemetr", string ans2 = "Lui Paster")
+    Question1 Question1(string ans3 = "Nicola Tesla", string ans4 = "Charls Babbidj")
+};
+class Question3:Question1{
+public:
+    Question1 Question1(string question = "And which Scottish-born American inventor was the first to get a patent for a phone?",\
+    string ans1 = "Alexandr Greyam Bell", string ans2 = "Samyel Morse")
+    Question1 Question1(string ans3 = "Stiv Jobs", string ans4 = "Alexandr Landau")
+};
+class Question4:Question1{
+public:
+    Question1 Question1(string question = "Who discovered electricity after experimenting with a kite during a storm?", \
+    string ans1 = "Benjamin Franklin", string ans2 = "Nicola Tesla")
+    Question1 Question1(string ans3 = "Tomas Edison", string ans4 = "Albert Einstein")
+};
+class Question5:Question1{
+public:
+    Question1 Question1(string question = "Which French virologist discovered HIV in 1983?", string ans1 = "Luk Montanie", \
+    string ans2 = "Lui Paster")
+    Question1 Question1(string ans3 = "Samyel Morse", string ans4 = "Gotfried Leibniz")
+};
+void test(){
     cout << "Hi, what's your name?";
-    cin >> name;
+    cin >> Question1.name;
     cout << "Welcome, " << name << endl << " Let's check your science knowledge" << endl;
-    cout << "What scientist wrote the book <Origin of species>?" << endl;
-    cout << "Charls Darwin" << endl << "Aristotel" << endl << "Thomas Edison" << endl << "Alexandr Greyam Bell" << endl;
-    cin >> ans;
-    while(ans != "Charls Darwin"){
-        cout << "Wrong, try again" << endl;
-        cin >> ans;
-    }
-    cout << "Well, lets continue" << endl;
-    count++;
-    cout << "This Belgian Catholic priest was the first to propose the big Bang theory" << endl;
-    cout << "Lui Paster" << endl << "Nicola Tesla" << endl << "Charls Babbidj" << endl << "Jorj Lemetr" << endl;
-    cin >> ans;
-    while(ans != "Jorj Lemetr"){
-        cout << "Wrong, try again" << endl;
-        cin >> ans;
-    }
-    cout << "Well, lets continue" << endl;
-    count++;
-    cout << "And which Scottish-born American inventor was the first to get a patent for a phone?" << endl;
-    cout << "Samyel Morse" << endl << "Alexandr Greyam Bell" << endl << "Stiv Jobs" << endl << "Alexandr Landau" << endl;
-    cin >> ans;
-    while(ans != "Alexandr Greyam Bell"){
-        cout << "Wrong, try again" << endl;
-        cin >> ans;
-    }
-    cout << "Well, lets continue" << endl;
-    count++;
-    cout << "Who discovered electricity after experimenting with a kite during a storm?" << endl;
-    cout << "Nicola Tesla" << endl << "Tomas Edison" << endl << "Albert Einstein" << endl << "Benjamin Franklin" << endl;
-    cin >> ans;
-    while(ans != "Benjamin Franklin"){
-        cout << "Wrong, try again" << endl;
-        cin >> ans;
-    }
-    cout << "Well, lets continue" << endl;
-    count++;
-    cout << "Which French virologist discovered HIV in 1983?" << endl;
-    cout << "Luk Montanie" << endl << "Lui Paster" << endl << "Samyel Morse" << endl << "Gotfried Leibniz" << endl;
-    cin >> ans;
-    while(ans != "Luk Montanie"){
-        cout << "Wrong, try again" << endl;
-        cin >> ans;
-    }
-    cout << "Well, lets continue" << endl;
-    count++;
-    cout << "Congratulation, you answer all  " << count << " question, you know history well" << endl;
+    Question1.foo();
+    Question2.foo();
+    Question3.foo();
+    Question4.foo();
+    Question5.foo();
+    cout << "Congratulation, you answer all  " << Question1.count << " question, you know history well" << endl;
     cout << "See you next time, " << name << ')';
 }
