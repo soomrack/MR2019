1b5eda13701f90d7a793fdd3a9b54b9dd16e9e3c
Anferhe <55059447+Anferhe@users.noreply.github.com>
2019-11-11 15:48:16 +0300

    Add files via upload

--- ShkabaraYA/Тест/Header1.cpp
+++ ShkabaraYA/Тест/Header1.cpp
@@ -1,14 +1,10 @@
 #include "Header1.h"
 #include <stdio.h>
 #include <string.h>
-#include <Windows.h>
 
 
 void test(const char Question[], const char trueAnswer[]) {
 	char Answer[20];
-
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
 
 	printf("%s\n", Question);
 	scanf_s("%s", Answer, 20);
