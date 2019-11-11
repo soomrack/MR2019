d2730eae8d4d341df82878b1a0b64894f2223eda
ImAllergicToFish <nne_leps@mail.ru>
2019-11-11 16:51:19 +0300

    Add sumator library for fun

---
+++ YakimenkoGK/LibExp_Yakimenko/cmake-build-debug/CMakeFiles/LibExp_Yakimenko.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LibExp_Yakimenko.dir/main.cpp.obj"
+  "CMakeFiles/LibExp_Yakimenko.dir/sumator.cpp.obj"
+  "LibExp_Yakimenko.pdb"
+  "LibExp_Yakimenko.exe"
+  "LibExp_Yakimenko.exe.manifest"
+  "libLibExp_Yakimenko.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LibExp_Yakimenko.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ YakimenkoGK/LibExp_Yakimenko/sumator.h
@@ -0,0 +1,8 @@
+//
+// Created by nne_l on 11.11.2019.
+//
+
+#ifndef LIBEXP_YAKIMENKO_SUMATOR_H
+#define LIBEXP_YAKIMENKO_SUMATOR_H
+int sum();
+#endif //LIBEXP_YAKIMENKO_SUMATOR_H
