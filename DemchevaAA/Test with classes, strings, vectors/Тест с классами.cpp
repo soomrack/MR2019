0fa1245483956b6084c44288e78d7a2791844ff4
Александра <ademtcheva@gmail.com>
2019-11-12 21:31:17 +0400

    Test with classes/strings/vectors

--- DemchevaAA/Test with classes, strings, vectors/Тест с классами.cpp
+++ DemchevaAA/Test with classes, strings, vectors/Тест с классами.cpp
@@ -1,28 +1,41 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <algorithm>
 #include <string>
+#include <vector>
 using namespace std;
 
-//char x1, x2, x3, x4;
+
 int SB = 0, SW = 0, RZ = 0, LX = 0;
+
+vector <string> Questions = { "1. Что вы любите делать в свободное время ? \n 1) спать \n 2) гулять \n 3) мечтать \n 4) смотреть аниме \n" ,
+							"2.Какой ваш любимый цвет ? \n 1) розовый \n 2) красный \n 3) желтый \n 4) влюбленной жабы \n",
+							"3.Лучшее ваше качество ? \n 1)мечтательность \n 2) доброта \n 3) сила духа \n 4) саркастичность \n",
+							"4.Какой  у вас напарник ? \n 1) конь \n 2) семь гномов \n 3) хамелеон \n 4) корги \n " };
+
+
+vector <string> Conclude = { "Поздравляем, Вы Спящая красавица, берегитесь игл и ... сладких снов\n\n",
+							"Поздравляем, Вы Белоснежка, остерегайтесь яблок\n\n",
+							"Поздравляем, Вы Рапунцель, вооружитесь сковородкой и НИКОГДА не ровняйте кончики\n\n",
+							"Вы не принцесса, вы Леха, психологический возраст 73\n\n" };
 
 class TestQuestion
 {
+	
 public:
 	char answer;
-	
+
 	void OutputQuestion(string message)
 	{
 		cout << message;
 	}
 	char GetAnswer()
-	{		
+	{
 		cin >> answer;
 		return answer;
 	}
 	int CountPoints(int a = 0)
 	{
+		
 		while (a < 1)
 		{
 			switch (answer)
@@ -53,83 +66,55 @@ public:
 		a = 0;
 		return  SB, SW, RZ, LX;
 	}
-	
-	string Result() 
+
+	string Result()
 	{
 		string Conclusion;
+	
 		if (max(max(SB, SW), max(RZ, LX)) == SB)
 		{
-			Conclusion = "Поздравляем, Вы Спящая красавица, берегитесь игл и ... сладких снов\n\n";
+			Conclusion = Conclude[0];
 		}
 		if (max(max(SB, SW), max(RZ, LX)) == SW)
 		{
-			Conclusion = "Поздравляем, Вы Белоснежка, остерегайтесь яблок\n\n";
+			Conclusion = Conclude[1];
 		}
 		if (max(max(SB, SW), max(RZ, LX)) == RZ)
 		{
-			Conclusion = "Поздравляем, Вы Рапунцель, вооружитесь сковородкой и НИКОГДА не ровняйте кончики\n\n";
+			Conclusion = Conclude[2];
 		}
 		if (max(max(SB, SW), max(RZ, LX)) == LX)
 		{
-			Conclusion = "Вы не принцесса, вы Леха, психологический возраст 73\n\n";
+			Conclusion = Conclude[3];
 		}
-	return Conclusion;
+		return Conclusion;
 	}
 };
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+
 	TestQuestion question1;
-	question1.OutputQuestion("1. Что вы любите делать в свободное время? \n 1) спать \n 2) гулять \n 3) мечтать \n 4) смотреть аниме \n");
+	question1.OutputQuestion(Questions[0]);
 	question1.GetAnswer();
 	question1.CountPoints();
 
 	TestQuestion question2;
-	question2.OutputQuestion("2.Какой ваш любимый цвет? \n 1) розовый \n 2) красный \n 3) желтый \n 4) влюбленной жабы \n");
+	question2.OutputQuestion(Questions[1]);
 	question2.GetAnswer();
 	question2.CountPoints();
 
+	TestQuestion question3;
+	question3.OutputQuestion(Questions[2]);
+	question3.GetAnswer();
+	question3.CountPoints();
+
+	TestQuestion question4;
+	question4.OutputQuestion(Questions[3]);
+	question4.GetAnswer();
+	question4.CountPoints();
+
 	TestQuestion Princess;
-	cout<< Princess.Result();
-
-	/*cout << "1. Что вы любите делать в свободное время? \n 1) спать \n 2) гулять \n 3) мечтать \n 4) смотреть аниме \n";
-	
-	cout << "2.Какой ваш любимый цвет? \n 1) розовый \n 2) красный \n 3) желтый \n 4) влюбленной жабы \n";
-	
-	cout << "3.Лучшее ваше качество? \n 1)мечтательность \n 2) доброта \n 3) сила духа \n 4) саркастичность \n";
-	
-	cout << "4.Какой  у вас напарник? \n 1) конь \n 2) семь гномов \n 3) хамелеон \n 4) корги \n";
-	
-
-
-	
-	if (max(max(SB, SW), max(RZ, LX)) == SB)
-	{
-		cout << "Поздравляем, Вы Спящая красавица, берегитесь игл и ... сладких снов\n\n";
-	}
-	if (max(max(SB, SW), max(RZ, LX)) == SW)
-	{
-		cout << "Поздравляем, Вы Белоснежка, остерегайтесь яблок\n\n";
-	}
-	if (max(max(SB, SW), max(RZ, LX)) == RZ)
-	{
-		cout << "Поздравляем, Вы Рапунцель, вооружитесь сковородкой и НИКОГДА не ровняйте кончики\n\n";
-	}
-	if (max(max(SB, SW), max(RZ, LX)) == LX)
-	{
-		cout << "Вы не принцесса, вы Леха, психологический возраст 73\n\n";
-	}*/
+	cout << Princess.Result();
 }
-
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
