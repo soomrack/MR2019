ccaf075ba407b6b2368d6392c278ba7ad77bc285
Антон Ларионов <statskey.sovetnik@gmail.com>
2019-11-23 21:30:11 +0300

    Fix last task

--- LarionovAA/Task with classes, vector, template/Test_with_ClaVecTem.cpp
+++ LarionovAA/Task with classes, vector, template/Test_with_ClaVecTem.cpp
@@ -1,6 +1,4 @@
-﻿// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
-
+﻿
 #include "QuestionTest.h"
 
 int main()
