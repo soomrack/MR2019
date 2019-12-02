a3f32f726a7371224168c9ff563968de85e99be3
Ярослав Шкабара <ananas617@mail.ru>
2019-12-02 16:35:18 +0300

    1

--- SkabaraYA/TestWithClasses/TestWithClasses.cpp
+++ SkabaraYA/TestWithClasses/TestWithClasses.cpp
@@ -1,34 +1,34 @@
-﻿#include <ctime>
-#include <iostream>
+﻿#include <iostream>
 #include <string>
-#include <vector>
 #include <windows.h>
+using namespace std;
+
 int score = 0;
 
-void answer_chek(std::string right_answer, std::string answer) {
+void answer_chek(string right_answer, string answer) {
 
 	if (answer != right_answer) {
-		std::cout << "Неверно" << '\n' << '\n';
+		cout << "Неверно" << '\n' << '\n';
 	}
 	else {
 		score++;
-		std::cout << "Верно\n" << '\n' << '\n';
+		cout << "Верно\n" << '\n' << '\n';
 	}
 }
 
 class Question
 {
 public:
-	std::string question;
-	std::string answers;
-	std::string right_answer;
+	string question;
+	string answers;
+	string right_answer;
 
 	void quest()
 	{
-		std::string users_answer;
-		std::cout << question << '\n';
-		std::cout << answers << '\n';
-		std::cin >> users_answer;
+		string users_answer;
+		cout << question << '\n';
+		cout << answers << '\n';
+		cin >> users_answer;
 		answer_chek(right_answer, users_answer);
 	}
 };
@@ -47,10 +47,8 @@ int main()
 	Question q3{ "Год крещения Руси", "1.2020  2.988  3.0  4.666", "2" };
 	q3.quest();
 
-	
-
-	std::cout << '\n' << "Ваш результат:\n";
-	std::cout << score << '\n' << '\n';
+	cout << '\n' << "Ваш результат:\n";
+	cout << score << '\n' << '\n';
 
 	system("pause");
 	return 0;
