ed4564843f5ad7933cb7c015957cb3e2578c0995
Fuxtrel <a99som@yandex.ru>
2019-12-09 16:35:21 +0300

    Success add library:)

--- SomovAS/main.cpp
+++ SomovAS/main.cpp
@@ -1,7 +1,8 @@
 #include "test.h"
-#include "../YakimenkoGK/LibExp_Yakimenko/sumator.h"
+#include "../YakimenkoGK/LibExp_Yakimenko/sumator.h
 using namespace std;
 int main(){
+    int a;
  //   test();
  sum();
 }
