10f14ae21e75631841ebcf0b9cb8c176d4548374
arhipov-ae <awa123-a@mail.ru>
2019-12-09 15:26:38 +0300

    Changed stdout

--- ArhipovAE/main.c
+++ ArhipovAE/main.c
@@ -15,7 +15,7 @@ int main()
     do {
         reset = 0;
         printf("When was the first world war?\n");
-        printf("1. 1913 - 1918\n");
+        printf("1. 1913 - 1928\n");
         printf("2. 1914 - 1919\n");
         printf("3. 1914 - 1918\n");
         printf("4. 1912 - 1916\n");
