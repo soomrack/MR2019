b0a415fd6b1b11dab57aba051e816e1a78e2e187
Motvik <55060052+Motvik@users.noreply.github.com>
2019-12-16 14:47:31 +0300

    Put the class in the header file

--- KulikovMM/NewClassQ/NewClassQ.cpp
+++ KulikovMM/NewClassQ/NewClassQ.cpp
@@ -1,43 +1,6 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+#include "HNewClassQ.h"
 using namespace std;
-
-class Queue
-{
-private:
-	enum { MAX = 10 };
-	int qu[MAX];
-	int tail;
-	int top;
-public:
-	Queue()
-	{
-		tail = -1;
-		top = 0;
-	}
-	int push(int n)
-	{
-		++tail;
-		qu[tail] = n;
-		return n;
-	}
-	int pop()
-	{
-		int temp;
-		temp = qu[top];
-		top++;
-		return temp;
-	}
-	void viewqueue()
-	{
-		int j=top;
-		cout << endl;
-			for (j; j <= tail; j++)
-			{
-				cout << qu[j] << endl;
-			}
-	}
-};
 
 int main()
 {
