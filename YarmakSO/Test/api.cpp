abb62de37bb5b766b4b43010628d81fa682559cf
soomrack <soomrack@gmail.com>
2019-12-26 15:31:20 +0300

    Update api.cpp

--- YarmakSO/Test/api.cpp
+++ YarmakSO/Test/api.cpp
@@ -8,7 +8,7 @@ void Ticket::set_question(const std::string question) {
 }
 Ticket::Ticket(const Ticket& ticket) {
 	question = ticket.question;
-	answers = ticket.answers;
+	аnswers = ticket.answers;
 
 }
 
