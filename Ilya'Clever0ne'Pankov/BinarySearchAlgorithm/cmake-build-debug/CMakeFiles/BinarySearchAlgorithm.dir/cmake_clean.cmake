b4fa3f15d761d37bbb21b4f3aa31b571861938e6
ilya'Clever0ne'Pankov <ilya-pankoff@yandex.ru>
2020-02-10 05:01:18 +0300

    Revert "Add Binary Search Algorithm and unit test for checking if it's working correctly. Everyone! Feel free to use it to check your own library with binary search."
    This reverts commit cf08367f

---
+++ Ilya'Clever0ne'Pankov/BinarySearchAlgorithm/cmake-build-debug/CMakeFiles/BinarySearchAlgorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "BinarySearchAlgorithm.exe"
+  "BinarySearchAlgorithm.exe.manifest"
+  "BinarySearchAlgorithm.pdb"
+  "CMakeFiles/BinarySearchAlgorithm.dir/test.cpp.obj"
+  "libBinarySearchAlgorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BinarySearchAlgorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
