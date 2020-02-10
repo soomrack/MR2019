7269f6fb386c41fdeb381d39d453606cf388998c
Fuxtrel <a99som@yandex.ru>
2020-02-10 11:45:24 +0300

    Бинарный поиск

---
+++ SomovAS/BinarySearch/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void binSearch(vector<int> arr, int find_number){
+    int left = 0;
+    int right = 20;
+    int mid;
+    bool flag = false;
+    while(left <= right && flag == false){
+        mid = (left + right) / 2;
+        if(arr[mid] == find_number){
+            flag = true;
+        }
+        if(arr[mid] > find_number){
+            right = mid - 1;
+        }else{
+            left = mid + 1;
+        }
+    }
+    if(flag){
+        cout << endl << mid;
+    }else{
+        cout << endl << "Netu";
+    }
+}
+
+int main() {
+    vector<int> arr;
+    for(int i = 0; i < 20; i++){
+        arr.push_back(rand() % 15 +1);
+        cout << arr[i] << ' ';
+    }
+    cout << endl;
+    sort(arr.begin(), arr.end());
+    for(int i : arr){
+        cout << i << ' ';
+    }
+    binSearch(arr, 5);
+}
