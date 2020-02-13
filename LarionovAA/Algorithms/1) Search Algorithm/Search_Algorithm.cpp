2433a004f12b5f74e0dec1fdb89cfe36f7e282bb
Anton Larionov <55048058+TonyCooT@users.noreply.github.com>
2020-02-14 00:03:23 +0300

    Update Search_Algorithm.cpp

--- LarionovAA/Algorithms/1) Search Algorithm/Search_Algorithm.cpp
+++ LarionovAA/Algorithms/1) Search Algorithm/Search_Algorithm.cpp
@@ -1,5 +1,3 @@
-﻿// This is a personal academic project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 
 #include <iostream>
 #include <cstdlib>
