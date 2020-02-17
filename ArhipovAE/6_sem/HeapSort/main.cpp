1cb007a3333dca31d010825723f170e8d826b126
arhipov-ae <awa123-a@mail.ru>
2020-02-17 12:00:44 +0300

    Add HeapSort

--- ArhipovAE/6_sem/HeapSort/main.cpp
+++ ArhipovAE/6_sem/HeapSort/main.cpp
@@ -22,13 +22,13 @@ void BinaryHeap(int arr[], const int size, int index){
 }
 
 void heapSort(int arr[], const int size){
-    for (int i = size / 2 - 1; i >= 0; i--)
-        BinaryHeap(arr, size, i);
+    for (int index = size / 2 - 1; index >= 0; index--)
+        BinaryHeap(arr, size, index);
 
-    for (int i = size - 1; i >= 0; i--){
-        swap(arr[0], arr[i]);
+    for (int index = size - 1; index >= 0; index--){
+        swap(arr[0], arr[index]);
 
-        BinaryHeap(arr, i, 0);
+        BinaryHeap(arr, index, 0);
     }
 }
 
