31477f0fecad2312f0201ae835f818a456b1e248
Яков Каретный <55059033+DonKiRav@users.noreply.github.com>
2020-02-17 13:11:24 +0300

    Add files via upload

--- DonKiRav/Merge Sort/main.c
+++ DonKiRav/Merge Sort/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void merge_sort(int * arr, int * buff, int left_border, int right_border) {
+void merge_sort(int * arr, int * buff, const int left_border, const int right_border) {
 
     if (right_border <= left_border)
         return;
@@ -11,31 +11,31 @@ void merge_sort(int * arr, int * buff, int left_border, int right_border) {
     for (int k = left_border; k <= right_border; k++)
         buff[k] = arr[k];
 
-    int i = left_border, j = middle + 1;
+    int left = left_border, right = middle + 1;
     for (int k = left_border; k <= right_border; k++) {
-        if (i > middle) {
-            arr[k] = buff[j];
-            j++;
-        } else if (j > right_border) {
-            arr[k] = buff[i];
-            i++;
-        } else if (buff[j] < buff[i]) {
-            arr[k] = buff[j];
-            j++;
+        if (left > middle) {
+            arr[k] = buff[right];
+            right++;
+        } else if (right > right_border) {
+            arr[k] = buff[left];
+            left++;
+        } else if (buff[right] < buff[left]) {
+            arr[k] = buff[right];
+            right++;
         } else {
-            arr[k] = buff[i];
-            i++;
+            arr[k] = buff[left];
+            left++;
         }
     }
 }
 
 int main() {
-    unsigned int size1 = 6;
+    int size1 = 6;
     int arr1[6] = {12, 5, 77, 22, 5, 647};
     int buf[size1];
     merge_sort(arr1, buf, 0, size1);
 
-    for (unsigned int i = 0; i < size1; i++)
+    for (int i = 0; i < size1; i++)
     {
         printf("%d ", arr1[i]);
     }
