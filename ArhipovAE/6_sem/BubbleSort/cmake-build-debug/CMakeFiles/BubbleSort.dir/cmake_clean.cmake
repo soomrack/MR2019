9dbcfcd3407006b87d2d6d832bf77b1923ec7b2e
arhipov-ae <awa123-a@mail.ru>
2020-02-17 10:49:07 +0300

    BinaryHeap

---
+++ ArhipovAE/6_sem/BubbleSort/cmake-build-debug/CMakeFiles/BubbleSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BubbleSort.dir/main.cpp.obj"
+  "BubbleSort.pdb"
+  "BubbleSort.exe"
+  "BubbleSort.exe.manifest"
+  "libBubbleSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BubbleSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ArhipovAE/6_sem/search/cmake-build-debug/CMakeFiles/search.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search.dir/main.cpp.obj"
+  "search.pdb"
+  "search.exe"
+  "search.exe.manifest"
+  "libsearch.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
