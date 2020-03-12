bed759ea46dabb0849fde0ca5b003dbdbbc55547
Sokolov-Denis <52282102+perchess@users.noreply.github.com>
2020-03-12 14:12:06 +0300

    upd class_tre.cpp
    Added delete method

--- SOKOLOV-D-A/class_tree.cpp
+++ SOKOLOV-D-A/class_tree.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 
+
+
 class Node
 {
 public:
@@ -14,7 +16,7 @@ public:
 		, left(nullptr)
 		, right(nullptr) {}
 
-	Node(void* data, int key)
+	Node(void *data, int key)
 		: key(key)
 		, data(new void* (data))
 		, left(new Node())
@@ -22,7 +24,7 @@ public:
 	{}
 	Node(const Node& other)
 		: key(other.key)
-		, data(new void* (other.data))
+		, data(new void*(other.data))
 		, left(other.left)
 		, right(other.right)
 	{}
@@ -32,17 +34,15 @@ public:
 };
 
 
-
 class Tree : public Node
 {
-	Node* root;
+	Node * root;
 
-	Node* add_data(void* data, const int key, Node* root)
+	Node * add_data(void* data, const int key, Node* root)
 	{
-
+		Node* child = new Node(data, key);
 		if (root->key == 0)
 		{
-			Node* child = new Node(data, key);
 			root = child;
 			return root;
 		}
@@ -58,6 +58,22 @@ class Tree : public Node
 		}
 		return root;
 
+	}
+
+	Node* repairTree(Node* root)
+	{
+		if (root->right != nullptr)
+		{
+			 root = root->right;
+			 return root;
+		}
+			
+		else
+		{
+			root = root->left;
+			return root;
+		}
+			
 	}
 public:
 	Tree()
@@ -80,13 +96,32 @@ public:
 	}
 
 
+	Node* findParent(Node* node, Node* root)
+	{
+		if (node == NULL)
+			return 0;
+
+		
+		if (node == root->right || node == root->left) {
+			std::cout << "parent";
+			return root;
+		}
+
+		else {
+			findParent(node, root->left);
+			findParent(node, root->right);
+		}
+	}
+
+
 	int add_data(void* data, const int key)
 	{
-		if (this->root == nullptr)
+		Node* child = new Node(data, key);
+
+		if (this->root == nullptr )
 		{
-			Node* child = new Node(data, key);
 			this->root = child;
-			return 0;
+			return 1;
 		}
 		if (key <= this->root->key)
 		{
@@ -97,13 +132,64 @@ public:
 		{
 			this->root->right = add_data(data, key, this->root->right);
 		}
-		return 0;
+		return 1;
+
+		
 	}
 
 
+	Node* minValueNode(Node* node)
+	{
+		Node* current_node = node;
+
+		
+		while (current_node && current_node->left != NULL)
+			current_node = current_node->left;
+
+		return current_node;
+	}
+
+
+	//Могут быть ошибки, не доделал) каюсь!
+
 	void delete_data(const int key)
 	{
-		delete search(this->root, key);
+		Node* del_node = search(this->root, key);
+		Node* new_node;
+		Node* parent_del_node;
+
+		if (root == nullptr)
+			std::cout << "Tree is empty";
+
+		else if (del_node->left->key == 0)
+		{
+			new_node = del_node->right;
+			parent_del_node = findParent(del_node, root);
+			parent_del_node->right = new_node;
+			delete del_node;
+		}
+
+		else if (del_node->right->key == 0)
+		{
+			new_node = del_node->left;
+			parent_del_node = findParent(del_node, root);
+			parent_del_node->left = new_node;
+			delete del_node;
+		}
+
+		else
+		{
+			new_node = minValueNode(del_node);
+			Node* parent_new_node = findParent(new_node, root);
+			parent_del_node = findParent(del_node, root);
+			
+			new_node->left = del_node->left;
+			new_node->right = del_node->right;
+			parent_del_node->left = new_node;
+			parent_new_node->left = new_node->right;
+			delete del_node;
+		}
+
 	}
 };
 
@@ -118,16 +204,20 @@ int main()
 	int b = 5 * 5;
 	int c = 6;
 	int d = 9;
+	int j = 66;
 	Tree tree;
 	tree.add_data(&a, 5);
-	tree.add_data(&b, 25);
+	tree.add_data(&b,25);
 	tree.add_data(&c, 4);
 	tree.add_data(&d, 50);
-	tree.delete_data(4);
-
+	tree.add_data(&j, 23);
+	tree.delete_data(25);
+	
 	system("pause");
 
+
+
 	return 0;
+
+
 }
-
-
