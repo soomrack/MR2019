2adac21a275e77caa2c5e61fd7579cbe90d6d7d6
Sokolov-Denis <52282102+perchess@users.noreply.github.com>
2020-04-09 21:41:13 +0300

    upd B-Tree.cpp
    добавил метод поиска и ключ связал с данными

--- SOKOLOV-D-A/B-Tree.cpp
+++ SOKOLOV-D-A/B-Tree.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <map>
 const int M = 4;
 
 
 class B_Node
 {
 public:
-    B_Node(int key = 0);
+    B_Node(int key, void* data = nullptr);
 
     ~B_Node(){}
 
-
     // обход + вывод в консоль
     void print();
-
-    std::vector <B_Node*> childrens;
-    int key[M - 1];
-    int numberOfKeys;
 
     friend class B_Tree;
 private:
@@ -26,6 +22,11 @@ private:
     void shiftKeyRight();
     // метод делит узел на три 
     B_Node* divideNode();
+    void* search(int key);
+
+    std::vector <B_Node*> childrens;
+    std::vector < std::pair<int, void*>> key;
+    int numberOfKeys;
 };
 
 
@@ -35,13 +36,17 @@ public:
     B_Tree() {this->root = nullptr;}
     ~B_Tree(){}
     //метод добавления нового ключа в дерево
-    int addData(int key);
+    int addData(int key, void* data = nullptr);
+    //вывод дерева на экран
+    void print();
+    //Поиск элемента по ключу
+    void* search(int key);
 
     B_Node* root;
 
 private:
     // вспомогательный метод для рекурсии
-    int addData(int key, B_Node* root);
+    int addData(int key, B_Node* root, void* data = nullptr);
     //флаг для роста дерева вверх
     bool riseUpFlag{0};
     // метод, реализующий поднятие узла на уровень выше
@@ -50,11 +55,11 @@ private:
 };
 
 
-int B_Tree::addData(int key)
+int B_Tree::addData(int key, void* data)
 {
     if (!this->root)
     {
-        root = new B_Node(key);
+        root = new B_Node(key, data);
         return 0;
     }
                
@@ -62,23 +67,23 @@ int B_Tree::addData(int key)
     {
         for (size_t i = 0; i < M - 1; i++)
         {
-            if (key <= this->root->key[i])
+            if (key <= this->root->key.at(i).first)
             {
                 if (i != (M - 2))
                 {
                     this->root->shiftKeyRight();
-                    this->root->key[i] = key;
+                    this->root->key.at(i) = std::make_pair(key, data);
                 }
                 else
                 {
-                    this->root->key[i] = key;
+                    this->root->key.at(i) = std::make_pair(key, data);
                 }
                 break;
             }
             else
             {
-                if (this->root->key[i + 1] == 0)
-                    this->root->key[i + 1] = key;
+                if (this->root->key.at(i+1).first == 0)
+                    this->root->key.at(i+1) = std::make_pair(key, data);
             }
         }
         root->numberOfKeys++;
@@ -87,39 +92,39 @@ int B_Tree::addData(int key)
     else if (!this->root->canPush() && this->root->childrens.empty())
     {
         this->root->divideNode();
-        addData(key, this->root);
+        addData(key, this->root, data);
     }
     else
     {
-        addData(key, this->root);
+        addData(key, this->root, data);
     }
 }
 
-int B_Tree::addData(int key, B_Node* root)
+int B_Tree::addData(int key, B_Node* root, void* data)
 {
     
     if (!root->childrens.empty())
     { 
         for (size_t i = 0; i < M - 1; i++)
         {
-            if (key <= root->key[i])
+            if (key <= root->key.at(i).first)
             {
-                addData(key, root->childrens[i]);
+                addData(key, root->childrens[i], data);
                 if (riseUpFlag)
                 {
                     riseUpFlag = false;
                     riseUpNode(root->childrens[i], root);
 
-                    addData(key, root);
+                    addData(key, root, data);
                 }
                 return 0;
             }
             else
             {
-                if (root->key[i + 1] == 0 && i < M/2)
-                    addData(key, root->childrens[i + 1]);
+                if (i < M / 2 && root->key.at(i+1).first == 0)
+                    addData(key, root->childrens[i + 1], data);
                 else if (i == M/2)
-                    addData(key, root->childrens[i + 1]);
+                    addData(key, root->childrens[i + 1], data);
                 else
                 {
                     continue;
@@ -131,7 +136,7 @@ int B_Tree::addData(int key, B_Node* root)
                     riseUpNode(root->childrens[i + 1], root);
                     if (riseUpFlag)
                         continue;
-                    addData(key, root);
+                    addData(key, root, data);
                 }
                 return 0;
             }
@@ -144,23 +149,23 @@ int B_Tree::addData(int key, B_Node* root)
         {
             for (size_t i = 0; i < M - 1; i++)
             {
-                if (key <= root->key[i])
+                if (key <= root->key.at(i).first)
                 {
                     if (i != (M - 2))
                     {
                         root->shiftKeyRight();
-                        root->key[i] = key;
+                        root->key.at(i) = std::make_pair(key, data);
                     }
                     else
                     {
-                        root->key[i] = key;
+                        root->key.at(i) = std::make_pair(key, data);
                     }
                     break;
                 }
                 else
                 {
-                    if (root->key[i + 1] == 0)
-                        root->key[i + 1] = key;
+                    if (root->key.at(i+1).first == 0)
+                        root->key.at(i+1) = std::make_pair(key, data);
                 }
             }
 
@@ -184,19 +189,19 @@ int B_Tree::riseUpNode(B_Node* node, B_Node* root)
     {
         for (size_t i = 0; i < M - 1; i++)
         {
-            if (node->key[0] <= root->key[i])
+            if (node->key.at(0).first <= root->key.at(i).first)
             {
                 if (i != (M - 2))
                 {
                     root->shiftKeyRight();
-                    root->key[i] = node->key[0];
+                    root->key.at(i).first = node->key.at(0).first;
                     root->childrens[i] = node->childrens[i];
                     root->childrens[i + 1] = node->childrens[i + 1];
 
                 }
                 else
                 {
-                    root->key[i] = node->key[0];
+                    root->key.at(i).first = node->key.at(0).first;
                     root->childrens[i] = node->childrens[i];
                     root->childrens[i + 1] = node->childrens[i + 1];
                 }
@@ -204,9 +209,9 @@ int B_Tree::riseUpNode(B_Node* node, B_Node* root)
             }
             else
             {
-                if (root->key[i + 1] == 0)
+                if (root->key.at(i+1).first == 0)
                 {
-                    root->key[i + 1] = node->key[0];
+                    root->key.at(i+1).first = node->key.at(0).first;
                     root->childrens[i + 1] = node->childrens[0];
                     root->childrens.push_back(node->childrens[1]);
 
@@ -224,7 +229,7 @@ int B_Tree::riseUpNode(B_Node* node, B_Node* root)
     {
         root->divideNode();
         
-        if (node->key[0] <= root->key[0] && root == this->root)
+        if (node->key.at(0).first <= root->key.at(0).first && root == this->root)
             riseUpNode(node, root->childrens[0]);
         else if (root == this->root)
         {
@@ -240,13 +245,13 @@ int B_Tree::riseUpNode(B_Node* node, B_Node* root)
 }
 
 
-B_Node::B_Node(int key = 0)
+B_Node::B_Node(int key, void* data)
 {
     childrens.clear();
-    this->key[0] = key;
+    this->key.push_back(std::make_pair(key, data));
     for (size_t i = 1; i < M - 1; i++)
     {
-        this->key[i] = 0;
+        this->key.push_back(std::make_pair(0, nullptr));
     }
     numberOfKeys = 1;
 }
@@ -262,9 +267,9 @@ bool B_Node::canPush()
 
 B_Node* B_Node::divideNode()
 {
-    B_Node* main = new B_Node(this->key[(M / 2) - 1]);
-    main->childrens.push_back(new B_Node(this->key[0]));
-    main->childrens.push_back(new B_Node(this->key[M-2]));
+    B_Node* main = new B_Node(this->key.at((M / 2) - 1).first, this->key.at((M / 2) - 1).second);
+    main->childrens.push_back(new B_Node(this->key.at(0).first, this->key.at(0).second));
+    main->childrens.push_back(new B_Node(this->key.at(M-2).first, this->key.at(M - 2).second));
     if (!this->childrens.empty())
     {
         for (size_t sourceIterator = 0, destinationIterator = 0;
@@ -278,12 +283,37 @@ B_Node* B_Node::divideNode()
     return main;
 }
 
+void* B_Node::search(int key)
+{
+    if (this->childrens.empty())
+    {
+        for (auto i = 0; i < M - 1; i++)
+            if (key == this->key.at(i).first)
+                return this->key.at(i).second;
+    }
+    for (auto i = 0; i < M - 1; i++)
+    {
+        if (key < this->key.at(i).first)
+        {
+            this->childrens[i]->search(key);
+            break;
+        }
+        else
+        {
+            if (this->key.at(i+1).first != 0 && (i + 1) != M)
+                continue;
+            this->childrens[i + 1]->search(key);
+            break;
+        }
+    }
+}
+
 
 void B_Node::shiftKeyRight()
 {
     for (size_t i = this->numberOfKeys; i > 0; i--)
     {
-        this->key[i] = this->key[i - 1];
+        this->key.at(i).first = this->key.at(i - 1).first;
         if (!this->childrens.empty())
         {
             if (this->childrens.size() < M)
@@ -298,39 +328,80 @@ void B_Node::shiftKeyRight()
 void B_Node::print()
 {
     size_t i;
-    for (i = 0; i < M - 1; i++)
+    for (i = 0; i < M-1; i++)
     {
-        if (!this->childrens.empty())
+        if (i < this->childrens.size())
+        {
             childrens[i]->print();
-        std::cout << " " << key[i];
+            //std::cout << "\n";
+        }
+        if (key.at(i).first != 0)
+        {
+            std::cout << " " << key.at(i).first;
+            //std::cout << "\n";
+        }
+            
     }
-    if (!this->childrens.empty())
+    std::cout << "\n";
+    if (i < this->childrens.size())
         childrens[i]->print();
+}
+
+void B_Tree::print()
+{
+    this->root->print();
+}
+
+void* B_Tree::search(int key)
+{
+    if (this->root->childrens.empty())
+    {
+        for (auto i = 0; i < M - 1; i++)
+            if (key == this->root->key.at(i).first)
+                return this->root->key.at(i).second;
+    }
+    for (auto i = 0; i < M - 1; i++)
+    {
+        if (key < this->root->key.at(i).first)
+        {
+            this->root->childrens[i]->search(key);
+            break;
+        }
+        else
+        {
+            if (this->root->key.at(i+1).first != 0 && (i+1) != M)
+                continue;
+            this->root->childrens[i+1]->search(key);
+            break;
+        }
+    }
 }
 
 int main()
 {
-    B_Tree first;
-    first.addData(1);
-    first.addData(2);
-    first.addData(3);
-    first.addData(4);
-    first.addData(17);
-    first.addData(31);
-    first.addData(7);
-    first.addData(9);
-    first.addData(11);
-    first.addData(13);
-    first.addData(16);
-    first.addData(19);
-    first.addData(26);
-    first.addData(27);
-    first.addData(96);
-    first.addData(99);
-    first.addData(97);
-    first.addData(14);
-    
-    first.root->print();
+    std::string data6 = "Putin";
+    B_Tree b_tree;
+    b_tree.addData(1);
+    b_tree.addData(2);
+    b_tree.addData(3); 
+    b_tree.addData(4);
+    b_tree.addData(17);
+    b_tree.addData(31);
+    b_tree.addData(7);
+    b_tree.addData(9);
+    b_tree.addData(11);
+    b_tree.addData(13);
+    b_tree.addData(16);
+    b_tree.addData(19);
+    b_tree.addData(26);
+    b_tree.addData(27);
+    b_tree.addData(96, &data6);
+    b_tree.addData(99);
+    b_tree.addData(97);
+    b_tree.addData(14);
+    std::string* a = 0;
+    a = (std::string*)b_tree.search(96);
+    b_tree.print();
 
     system("pause");
     return 0;
