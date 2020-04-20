cce8f4c1d8c1d93c01d92f722272da5ff1e5bff0
dekoden <54942408+dekoden@users.noreply.github.com>
2020-04-20 14:35:46 +0300

    Создал B-дерево

---
+++ KozlovDA/b_tree/Btree.h
@@ -0,0 +1,41 @@
+#ifndef BTREE_H
+#define BTREE_H
+
+const int t = 4;
+
+class BNode
+{
+public:
+	int keys[2 * t - 1];
+	BNode* children[2 * t];
+	BNode* parent;
+	int count;
+	bool leaf;
+public:
+	BNode();
+	~BNode() {};
+	void print();
+};
+
+class Tree
+{
+public:
+	BNode* root;
+public:
+	Tree() { root = nullptr; };
+	~Tree() {};
+	BNode* find_node(int key, BNode* root);
+	void add_data(int key);
+	void delete_tree(BNode* root);
+	void print();
+private:
+	int binary_search(int* a, int first, int last, const int x);
+	BNode* search_parent(int key);
+	void insert_to_node(int key, BNode* node);
+	void sort_node(BNode* node);
+	BNode* find_position_for_new_node(int key);
+	void restruct(BNode* node);
+	BNode* next_node_bsf(const bool restart);
+};
+
+#endif
