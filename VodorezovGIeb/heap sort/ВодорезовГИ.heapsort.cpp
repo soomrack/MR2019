da934ada6cf86e4ffdae106b0e40b4b7ec52739d
Gleb <55104272+Gleboss1@users.noreply.github.com>
2020-05-25 03:03:57 +0300

    добавляю сортировку кучей

---
+++ VodorezovGIeb/heap sort/ВодорезовГИ.heapsort.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+
+    int l = 2 * i + 1; // левый = 2*i + 1
+    int r = 2 * i + 2; // правый = 2*i + 2
+
+ // Если левый дочерний элемент больше корня
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    // Если самый большой элемент не корень
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+
+void heapSort(int arr[], int n)
+{
+    // Построение кучи (перегруппируем массив)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+
+int main()
+{
+    const int N = 6;
+    int a[N] = { 12, 11, 13, 5, 6, 7 };
+
+    heapSort(a, N);
+
+    cout << "After heap sort: \n";
+    printArray(a, N);
+}
